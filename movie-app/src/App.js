fdaa63135c2195944c57c4ea8fdca97d47dcd781
tiernanraftery <G00418395@atu.ie>
2024-10-04 08:49:01 +0000

    Made component folder and content file

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -5,7 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/*Prints hello world to the screen */}
         <h1>Hello World</h1>
+        {/*Prints time to the screen */}
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
       </header>
     </div>
   );

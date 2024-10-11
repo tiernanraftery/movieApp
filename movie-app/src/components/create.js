ea5d164d7b1ead12f15d18f18e4618f127249b89
tiernanraftery <G00418395@atu.ie>
2024-10-11 08:32:18 +0000

    Read/create made

---
+++ movie-app/src/components/create.js
@@ -0,0 +1,6 @@
+const Create = () => {
+    return <h3>Hello from Create component</h3>;
+  };
+  //create footer file
+  //made to modularize the app
+  export default Create;
---
+++ movie-app/src/components/read.js
@@ -0,0 +1,6 @@
+const read = () => {
+    return <h3>Hello from Read component</h3>;
+  };
+  //create read file
+  //made to modularize the app
+  export default read;

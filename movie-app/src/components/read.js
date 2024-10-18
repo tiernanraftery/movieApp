9416af0e2f201f51a2572cbc069616ecf4ae7110
tiernanraftery <G00418395@atu.ie>
2024-10-18 08:53:05 +0000

    added Axois and added usestate

--- movie-app/src/components/read.js
+++ movie-app/src/components/read.js
@@ -1,41 +1,28 @@
+//Read.js
+
 import Movies from "./Movies";
-const Read = () => {
-    //added a constant variable that holds  JSON data
-    const data = [
-        {
-            "Title": "Avengers: Infinity War",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Captain America: Civil War",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-        },
-        {
-            "Title": "World War Z",
-            "Year": "2013",
-            "imdbID": "tt0816711",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-        }
-    ];
+import { useEffect, useState } from "react";
+import axios from "axios";
 
+function Read() {
+  const [movies, setMovies] = useState([]);
 
-    return (
-        
-        <div>
-            <h3>Hello from Read component</h3>
+  useEffect(() => {
+    axios.get('https://jsonblob.com/api/jsonblob/1287718524221775872')
+      .then((response) => {
+        setMovies(response.data.movies);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-            <Movies myMovies={data}/>
-        </div>
-    );
+  return (
+    <div>
+      <h2>This is my Read Component.</h2>
+      <Movies myMovies={movies} />
+    </div>
+  );
+}
 
-};
-//create read file
-//made to modularize the app
 export default Read;
